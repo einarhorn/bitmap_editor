db2802481ddced945acc9aff16212e7dca2ce234
eahorn2 <eahorn2@illinois.edu>
2018-03-16 18:03:56 -0500

    Removed unnecessary params from MissingImageError

--- lib/exceptions/missing_image_exception.rb
+++ lib/exceptions/missing_image_exception.rb
@@ -1,9 +1,10 @@
 class MissingImageError < StandardError
 
-    def initialize(message, action)
-      # Call the parent's constructor to set the message
-      super(message)
+    EXCEPTION_MSG = "There is no image associated with the image editor"
+
+    def initialize()
+      super(EXCEPTION_MSG)
     end
-  
+
   end
   
