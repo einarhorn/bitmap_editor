0745df83b5efd1625ddcc7a9485be7724855c9ed
eahorn2 <eahorn2@illinois.edu>
2018-03-16 15:06:34 -0500

    draw_vertical + draw_horizontal + tests

--- lib/image.rb
+++ lib/image.rb
@@ -5,14 +5,18 @@ class Image
   MIN_ROW_SIZE = MIN_COL_SIZE = 1
   MAX_ROW_SIZE = MAX_COL_SIZE = 250
   DEFAULT_EMPTY_COLOR = 'O'
-  
+
+  # Error messages
+  OUT_OF_BOUNDS_ERR_MSG = "Row or col is out of image bounds"
+  OUT_OF_MAX_BOUNDS_ERR_MSG = "Row or col size is outside allowed range [1..250]"
+  INVALID_COLOR_ERR_MSG = "Color is not a capital letter"
+  INVALID_ROW_START_END_ORDER_ERR_MSG = "Expected start row to be less than end row"
+  INVALID_COL_START_END_ORDER_ERR_MSG = "Expected start column to be less than end column"
 
   # Initialize a blank image of rows x cols pixels
   def initialize(rows, cols)
     # Verify rows and cols are within allowed range [1..250]
-    if outside_max_image_bounds?(rows, cols)
-      raise ArgumentError, "Row or col size is outside allowed range [1..250]"
-    end
+    raise ArgumentError, OUT_OF_MAX_BOUNDS_ERR_MSG if outside_max_image_bounds?(rows, cols)
     
     # Set rows and cols
     @rows = rows
@@ -24,14 +28,14 @@ class Image
 
   # Color a specific pixel (row,col) with a color
   def color_pixel(row, col, color)
-    raise IndexError, "Row or col is out of image bounds" if outside_curr_image_bounds?(row, col)
-    raise ArgumentError, "Color is not a capital letter" if !is_capital_letter?(color)
+    raise IndexError, OUT_OF_BOUNDS_ERR_MSG if outside_curr_image_bounds?(row, col)
+    raise ArgumentError, INVALID_COLOR_ERR_MSG if !is_valid_color?(color)
     @image[row-1][col-1] = color
   end
 
   # Get the character representing the color at (row, col)
   def get_pixel(row, col)
-    raise IndexError, "Row or col is out of image bounds" if outside_curr_image_bounds?(row, col)
+    raise IndexError, OUT_OF_BOUNDS_ERR_MSG if outside_curr_image_bounds?(row, col)
     @image[row-1][col-1]
   end
 
@@ -40,6 +44,42 @@ class Image
     @image.each { |x|
       puts x.join(" ")
     }
+  end
+
+  # Draw a vertical segment of color in col between rowStart and rowEnd (inclusive)
+  # NOTE: Expects rowStart <= rowEnd
+  def draw_vertical_segment(col, rowStart, rowEnd, color)
+    is_out_of_bounds = outside_curr_image_bounds?(rowStart, col) || outside_curr_image_bounds?(rowEnd, col)
+    raise IndexError, OUT_OF_BOUNDS_ERR_MSG if is_out_of_bounds
+    raise ArgumentError, INVALID_COLOR_ERR_MSG if !is_valid_color?(color)
+    raise ArgumentError, INVALID_ROW_START_END_ORDER_ERR_MSG if rowStart > rowEnd
+    
+    # Convert start and end values to a continuous array
+    # e.g. rowStart = 1, rowEnd = 3 => rows = [1,2,3]
+    rows = (rowStart..rowEnd).to_a
+
+    # Update color for each pixel that needs to be changed
+    rows.each do |row|
+      @image[row-1][col-1] = color
+    end
+  end
+
+  # Draw a horizontal segment of color in row between colStart and colEnd (inclusive)
+  # NOTE: Expects colStart <= colEnd
+  def draw_horizontal_segment(row, colStart, colEnd, color)
+    is_out_of_bounds = outside_curr_image_bounds?(row, colStart) || outside_curr_image_bounds?(row, colEnd)
+    raise IndexError, OUT_OF_BOUNDS_ERR_MSG if is_out_of_bounds
+    raise ArgumentError, INVALID_COLOR_ERR_MSG if !is_valid_color?(color)
+    raise ArgumentError, INVALID_COL_START_END_ORDER_ERR_MSG if colStart > colEnd
+
+    # Convert start and end values to a continuous array
+    # e.g. colStart = 1, colEnd = 3 => cols = [1,2,3]
+    cols = (colStart..colEnd).to_a
+
+    # Update color for each pixel that needs to be changed
+    cols.each do |col|
+      @image[row-1][col-1] = color
+    end
   end
 
   private # Everything from here below are private methods
@@ -67,7 +107,7 @@ class Image
   end
 
   # Check if the character representing a color is a capital letter
-  def is_capital_letter?(color)
+  def is_valid_color?(color)
     color == color.upcase and color.length == 1
   end
 end
--- tests/image_test.rb
+++ tests/image_test.rb
@@ -4,7 +4,7 @@ require "test/unit"
  
 class TestImage < Test::Unit::TestCase
  
-  # Test Initialization of Image
+  # Tests: Initialization of Image
 
   def test_initialization_successful
     im = Image.new(2, 5)
@@ -46,7 +46,7 @@ class TestImage < Test::Unit::TestCase
     end
   end
 
-  # get_pixel
+  # Tests: get_pixel
 
   def test_get_pixel
     im = Image.new(2, 5)
@@ -79,7 +79,7 @@ class TestImage < Test::Unit::TestCase
     end
   end
 
-  # color_pixel
+  # Tests: color_pixel
 
   def test_color_pixel_successful
     im = Image.new(2, 5)
@@ -122,6 +122,193 @@ class TestImage < Test::Unit::TestCase
     # Combination of above
     assert_raise(ArgumentError) do
       im.color_pixel(1, 1, 'aBc')
+    end
+  end
+
+  # Tests: draw_vertical_segment
+  def test_draw_vertical_segment_successful
+    # Draw segment
+    im = Image.new(5, 5)
+    col = 3
+    startRow = 2
+    endRow = 5
+    im.draw_vertical_segment(col, startRow, endRow, 'A')
+
+    # Verify
+    expected_img_grid = Array[
+      ['O', 'O', 'O', 'O', 'O'],
+      ['O', 'O', 'A', 'O', 'O'],
+      ['O', 'O', 'A', 'O', 'O'],
+      ['O', 'O', 'A', 'O', 'O'],
+      ['O', 'O', 'A', 'O', 'O']
+    ]
+    assert_equal(expected_img_grid, im.image)
+  end
+
+  def test_draw_vertical_segment_throws_on_out_of_bounds
+    im = Image.new(5, 5)
+
+    valid_col = 2
+    valid_row_start = 2
+    valid_row_end = 3
+    valid_color = 'A'
+
+    invalid_col = 8
+    invalid_row_start = 6
+    invalid_row_end = 8
+
+    # Invalid column value
+    assert_raise(IndexError) do
+      im.draw_vertical_segment(
+        invalid_col,
+        valid_row_start, 
+        valid_row_end, 
+        valid_color
+      )
+    end
+
+    # Invalid row start value
+    assert_raise(IndexError) do
+      im.draw_vertical_segment(
+        valid_col,
+        invalid_row_start, 
+        valid_row_end, 
+        valid_color
+      )
+    end
+
+    # Invalid row end value
+    assert_raise(IndexError) do
+      im.draw_vertical_segment(
+        valid_col,
+        valid_row_start, 
+        invalid_row_end, 
+        valid_color
+      )
+    end
+  end
+
+  def test_draw_vertical_segment_throws_on_invalid_color_string
+    im = Image.new(2, 5)
+
+    # Lowercase character
+    assert_raise(ArgumentError) do
+      im.draw_vertical_segment(1, 1, 2, 'z')
+    end
+
+    # Multiple uppercase characters
+    assert_raise(ArgumentError) do
+      im.draw_vertical_segment(1, 1, 2, 'ABC')
+    end
+
+    # Combination of above
+    assert_raise(ArgumentError) do
+      im.draw_vertical_segment(1, 1, 2, 'aBc')
+    end
+  end
+
+  def test_draw_vertical_segment_throws_on_invalid_row_order
+    im = Image.new(2, 5)
+
+    # row_start > row_end should throw
+    row_start = 2
+    row_end = 1
+    assert_raise(ArgumentError) do
+      im.draw_vertical_segment(1, row_start, row_end, 'z')
+    end
+  end
+
+  # Tests: draw_horizontal_segment
+  
+  def test_draw_horizontal_segment_successful
+    # Draw segment
+    im = Image.new(5, 5)
+    row = 3
+    startCol = 2
+    endCol = 5
+    im.draw_horizontal_segment(row, startCol, endCol, 'A')
+
+    # Verify
+    expected_img_grid = Array[
+      ['O', 'O', 'O', 'O', 'O'],
+      ['O', 'O', 'O', 'O', 'O'],
+      ['O', 'A', 'A', 'A', 'A'],
+      ['O', 'O', 'O', 'O', 'O'],
+      ['O', 'O', 'O', 'O', 'O']
+    ]
+    assert_equal(expected_img_grid, im.image)
+  end
+
+  def test_draw_horizontal_segment_throws_on_out_of_bounds
+    im = Image.new(5, 5)
+
+    valid_row = 2
+    valid_col_start = 2
+    valid_col_end = 3
+    valid_color = 'A'
+
+    invalid_row = 8
+    invalid_col_start = 6
+    invalid_col_end = 8
+
+    # Invalid column value
+    assert_raise(IndexError) do
+      im.draw_horizontal_segment(
+        invalid_row,
+        valid_col_start, 
+        valid_col_end, 
+        valid_color
+      )
+    end
+
+    # Invalid row start value
+    assert_raise(IndexError) do
+      im.draw_horizontal_segment(
+        valid_row,
+        invalid_col_start, 
+        valid_col_end, 
+        valid_color
+      )
+    end
+
+    # Invalid row end value
+    assert_raise(IndexError) do
+      im.draw_horizontal_segment(
+        valid_row,
+        valid_col_start, 
+        invalid_col_end, 
+        valid_color
+      )
+    end
+  end
+
+  def test_draw_horizontal_segment_throws_on_invalid_color_string
+    im = Image.new(2, 5)
+
+    # Lowercase character
+    assert_raise(ArgumentError) do
+      im.draw_horizontal_segment(1, 1, 2, 'z')
+    end
+
+    # Multiple uppercase characters
+    assert_raise(ArgumentError) do
+      im.draw_horizontal_segment(1, 1, 2, 'ABC')
+    end
+
+    # Combination of above
+    assert_raise(ArgumentError) do
+      im.draw_horizontal_segment(1, 1, 2, 'aBc')
+    end
+  end
+
+  def test_draw_horizontal_segment_throws_on_invalid_row_order
+    im = Image.new(2, 5)
+
+    # col_start > col_end should throw
+    col_start = 2
+    col_end = 1
+    assert_raise(ArgumentError) do
+      im.draw_horizontal_segment(1, col_start, col_end, 'z')
     end
   end
 
